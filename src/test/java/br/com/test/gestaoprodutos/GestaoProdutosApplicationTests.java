d0d490d7837c10903de1a8fbe56fe1b76b66eae8
Herik dos Santos Lorenção <herik.lorencao@itixti.com.br>
2021-12-03 11:59:30 -0300

    adicionando arquivos iniciais

---
+++ src/test/java/br/com/test/gestaoprodutos/GestaoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.test.gestaoprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
